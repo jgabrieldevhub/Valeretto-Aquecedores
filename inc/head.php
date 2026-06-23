2802305a80ac820579ade75843e89848e128d181
Jorge <jorgegabriel.dev@gmail.com>
2026-06-22 21:45:49 -0300

    comeco do php com includes, head

---
+++ inc/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $page_title ?></title>
+    <link rel="stylesheet" href="./src/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./src/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="icon" href="./assets/img/transparent_png/cropped-Valeretto-favicon-192x192.png" sizes="192x192">
+</head>
