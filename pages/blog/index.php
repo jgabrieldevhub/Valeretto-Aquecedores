d1bd33f506fc55a76cad3602608c04597024ddd3
Jorge <jorgegabriel.dev@gmail.com>
2026-06-29 17:02:27 -0300

    estrutura inicial com html e includes php

---
+++ pages/blog/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php
+        $page_title = "Blog - Valeretto";
+        $css_page_link = "./style.css";
+        include("../../inc/head.php");
+    ?>
+<body>
+    <?php
+        include("../../inc/header.php");
+    ?>
+    <main>
+
+    </main>
+    <?php
+        include("../../inc/footer.php");
+    ?>
+
+    <?php
+        include("../../inc/wpp.php");
+    ?>
+
+    <?php 
+        $js_page_link = "./script.js";
+        include("../../inc/script.php");
+    ?>
+</body>
+</html>
