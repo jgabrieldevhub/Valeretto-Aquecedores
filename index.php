af54bb7ac1068d9d6b3a171454f1c04c83894b39
Jorge <jorgegabriel.dev@gmail.com>
2026-06-30 19:51:10 -0300

    criacao e execucao de array com php na secao de carrousel dos logos

--- index.php
+++ index.php
@@ -324,19 +324,27 @@
             </div>
         </section>
 
+        <?php
+            $infinity_carrousel_logos = [
+                ['./assets/img/transparent_png/Logo_Atual_Aquecedores.png','Logo_Atual_Aquecedores'],
+                ['./assets/img/transparent_png/Logo_Heliotek-1.png','Logo_Heliotek'],
+                ['./assets/img/transparent_png/Logo_Jacuzzi.png','Logo_Jacuzzi'],
+                ['./assets/img/transparent_png/Logo_MegraPress_.png','Logo_MegraPress'],
+                ['./assets/img/transparent_png/Logo_Mont-Serrat.png','Logo_Mont-Serrat'],
+                ['./assets/img/transparent_png/Logo_Orbitec.png','Logo_Orbitec'],
+                ['./assets/img/transparent_png/Logo_Rinnai.png','Logo_Rinnai'],
+                ['./assets/img/transparent_png/Logo_Rowa.png','Logo_Rowa'],
+                ['./assets/img/transparent_png/Logo_Schnelder.png','Logo_Schnelder'],
+            ];
+        ?>
+
         <section class="py-5">
             <div class="logos container">
                 <h4 class="text-center mb-5 fw-inter-bold text-uppercase">As Melhores <span class="text-orange">Marcas</span></h4>
                 <div class="logos-slide">
-                    <img src="./assets/img/transparent_png/Logo_Atual_Aquecedores.png" alt="Logo_Atual_Aquecedores">
-                    <img src="./assets/img/transparent_png/Logo_Heliotek-1.png" alt="Logo_Heliotek">
-                    <img src="./assets/img/transparent_png/Logo_Jacuzzi.png" alt="Logo_Jacuzzi">
-                    <img src="./assets/img/transparent_png/Logo_MegraPress_.png" alt="Logo_MegraPress">
-                    <img src="./assets/img/transparent_png/Logo_Mont-Serrat.png" alt="Logo_Mont-Serrat">
-                    <img src="./assets/img/transparent_png/Logo_Orbitec.png" alt="Logo_Orbitec">
-                    <img src="./assets/img/transparent_png/Logo_Rinnai.png" alt="Logo_Rinnai">
-                    <img src="./assets/img/transparent_png/Logo_Rowa.png" alt="Logo_Rowa">
-                    <img src="./assets/img/transparent_png/Logo_Schnelder.png" alt="Logo_Schnelder">
+                    <?php for($i = 0; $i < count($infinity_carrousel_logos); $i++) {?>
+                    <img src="<?php echo($infinity_carrousel_logos[$i][0])?>" alt="<?php echo($infinity_carrousel_logos[$i][1])?>">
+                    <?php }?>
                 </div>
             </div>
         </section>
