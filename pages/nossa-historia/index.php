e33bc0bd2368b9396b6097771fa2d90b9400c467
Jorge <jorgegabriel.dev@gmail.com>
2026-06-30 21:52:43 -0300

    estrutura inicial

---
+++ pages/nossa-historia/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <?php
+        $page_title = "Nossa História - Valeretto";
+        $css_page_link = "./style.css";
+        include("../../inc/head.php");
+    ?>
+<body>
+
+    <?php
+        include("../../inc/header.php");
+    ?>
+
+    <main>
+
+    </main>
+
+    <?php
+        include("../../inc/footer.php");
+    ?>
+
+    <?php
+        include("../../inc/wpp.php");
+    ?>
+
+    <?php
+        $js_page_link = "./script.js";
+        include("../../inc/script.php");
+    ?>
+    
+</body>
+</html>
